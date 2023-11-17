7450fd13f0393ce9186def119615eec389c618ee
Josef Leinweber <josef.leinweber@code.berlin>
2023-11-17 11:25:11 +0100

    trigger cloud build

--- main_application/src/config/settings/production.py
+++
@@ -1,8 +0,0 @@
-from src.config.settings.base import Settings
-
-
-class ProductionSettings(Settings):
-    DESCRIPTION: str = "Production Settings | Modified FastAPI Template"
-    ENVIRONMENT: str = "prod"
-    DEBUG: bool = False
-    TESTING: bool = False
--- main_application/src/models/schemas/profile_schema.py
+++
@@ -1,3 +0,0 @@
-import datetime
-import loguru
-import pydantic
