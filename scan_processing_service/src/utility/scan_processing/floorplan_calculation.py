e1f07fc044bade68c6d21e09a7974222a2f3f7d0
Josef Leinweber <josef.leinweber@code.berlin>
2023-12-04 16:04:02 +0100

    feat(floorplan_calc & workflow): action trigger changed to pr to trunk

---
+++ scan_processing_service/src/utility/scan_processing/floorplan_calculation.py
@@ -0,0 +1,60 @@
+import sys
+import functools
+import numpy as np
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import shapely.geometry as sg
+# from shapely.geometry import Polygon
+# from src.utility.scan_processing.helper_functions import alpha_shape
+
+
+def compute_floorplan(point_cloud):
+    floor_points = functools.reduce(
+        lambda a, b: a if len(a.points) > len(b.points) else b, segment_helper(point_cloud).values()
+    )
+
+    two_dimensinal_floor_points = np.array([[point[0], point[2]] for point in floor_points.points])
+
+    floor_edge_indexies = compute_edge_indexies(two_dimensinal_floor_points)
+
+    edge_dict = generate_edge_dict(two_dimensinal_floor_points, floor_edge_indexies)
+
+    sorted_edge_points = sort_edge_dict(edge_dict)
+
+    return Polygon(sorted_edge_points)
+
+
+def segment_helper(point_cloud, max_plane_idx=5, segments=None):
+    if segments is None:
+        segments = {}
+
+    if max_plane_idx == 0:
+        return segments
+
+    _, inliers = point_cloud.segment_plane(distance_threshold=0.2, ransac_n=3, num_iterations=1000)
+
+    segments[max_plane_idx - 1] = point_cloud.select_by_index(inliers, invert=False)
+
+    rest = point_cloud.select_by_index(inliers, invert=True)
+
+    return segment_helper(rest, max_plane_idx - 1, segments)
+
+
+def compute_edge_indexies(two_dimensinal_floor_points, alpha=0.25):
+    return [[i, j] for i, j in alpha_shape(two_dimensinal_floor_points, alpha=alpha, only_outer=True)]
+
+
+def generate_edge_dict(two_dimensinal_floor_points, floor_edge_indexies):
+    return {str(two_dimensinal_floor_points[i]): two_dimensinal_floor_points[j] for i, j in floor_edge_indexies}
+
+
+def sort_edge_dict(edge_dict):
+    sorted_edge_points = []
+    key = list(edge_dict.keys())[0]
+    for i in range(len(edge_dict)):
+        key = str(key)
+        value = edge_dict[key]
+        sorted_edge_points.append(value.tolist())
+        key = value
+    return sorted_edge_points
