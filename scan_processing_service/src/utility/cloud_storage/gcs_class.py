c46a9c251618c20bc89be494bc57c87c86600f54
Josef Leinweber <josef.leinweber@code.berlin>
2024-02-16 13:03:39 +0100

    fix(github action): gcloud key cannot be found

--- scan_processing_service/src/utility/cloud_storage/gcs_class.py
+++ scan_processing_service/src/utility/cloud_storage/gcs_class.py
@@ -46,28 +46,44 @@ class GCStorage:
 
 @lru_cache()
 def get_gcstorage() -> GCStorage:
-    try:
-        client = storage.Client()
+    # try:
+    #     client = storage.Client()
 
-    except Exception:
-        loguru.logger.error(
-            "Failed to authenticate with Google Cloud Storage by loading credentials from fiele. Trying to authenticate with environment variables."
-        )
-        client = storage.Client.from_service_account_info(
-            {
-                "type": os.environ.get("GCLOUD_TYPE"),
-                "project_id": os.environ.get("GCLOUD_PROJECT_ID"),
-                "private_key_id": os.environ.get("GCLOUD_PRIVATE_KEY_ID"),
-                "private_key": os.environ.get("GCLOUD_PRIVATE_KEY"),
-                "client_email": os.environ.get("GCLOUD_CLIENT_EMAIL"),
-                "client_id": os.environ.get("GCLOUD_CLIENT_ID"),
-                "auth_uri": os.environ.get("GCLOUD_AUTH_URI"),
-                "token_uri": os.environ.get("GCLOUD_TOKEN_URI"),
-                "auth_provider_x509_cert_url": os.environ.get("GCLOUD_AUTH_PROVIDER_CERT_URL"),
-                "client_x509_cert_url": os.environ.get("GCLOUD_CLIENT_CERT_URL"),
-                "universe_domain": "googleapis.com",
-            }
-        )
+    # except Exception:
+    #     loguru.logger.error(
+    #         "Failed to authenticate with Google Cloud Storage by loading credentials from fiele. Trying to authenticate with environment variables."
+    #     )
+    #     client = storage.Client.from_service_account_info(
+    #         {
+    #             "type": os.environ.get("GCLOUD_TYPE"),
+    #             "project_id": os.environ.get("GCLOUD_PROJECT_ID"),
+    #             "private_key_id": os.environ.get("GCLOUD_PRIVATE_KEY_ID"),
+    #             "private_key": os.environ.get("GCLOUD_PRIVATE_KEY"),
+    #             "client_email": os.environ.get("GCLOUD_CLIENT_EMAIL"),
+    #             "client_id": os.environ.get("GCLOUD_CLIENT_ID"),
+    #             "auth_uri": os.environ.get("GCLOUD_AUTH_URI"),
+    #             "token_uri": os.environ.get("GCLOUD_TOKEN_URI"),
+    #             "auth_provider_x509_cert_url": os.environ.get("GCLOUD_AUTH_PROVIDER_CERT_URL"),
+    #             "client_x509_cert_url": os.environ.get("GCLOUD_CLIENT_CERT_URL"),
+    #             "universe_domain": "googleapis.com",
+    #         }
+    #     )
+
+    client = storage.Client.from_service_account_info(
+        {
+            "type": os.environ.get("GCLOUD_TYPE"),
+            "project_id": os.environ.get("GCLOUD_PROJECT_ID"),
+            "private_key_id": os.environ.get("GCLOUD_PRIVATE_KEY_ID"),
+            "private_key": os.environ.get("GCLOUD_PRIVATE_KEY"),
+            "client_email": os.environ.get("GCLOUD_CLIENT_EMAIL"),
+            "client_id": os.environ.get("GCLOUD_CLIENT_ID"),
+            "auth_uri": os.environ.get("GCLOUD_AUTH_URI"),
+            "token_uri": os.environ.get("GCLOUD_TOKEN_URI"),
+            "auth_provider_x509_cert_url": os.environ.get("GCLOUD_AUTH_PROVIDER_CERT_URL"),
+            "client_x509_cert_url": os.environ.get("GCLOUD_CLIENT_CERT_URL"),
+            "universe_domain": "googleapis.com",
+        }
+    )
 
     return GCStorage(client)
 
